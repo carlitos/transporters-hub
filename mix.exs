80b8fea66a11fd5b802e2ed978b2142dac9529ab
Carlitos <carmoicast@gmail.com>
2024-01-31 18:48:48 -0600

    Add bcrypt for password creation

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule TransportersHub.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:guardian, "~> 2.3"},
-      {:bcrypt_elixir, "~> 3.1"}
+      {:bcrypt_elixir, "~> 3.1"},
+      {:erlsom, "~> 1.5"}
     ]
   end
 
